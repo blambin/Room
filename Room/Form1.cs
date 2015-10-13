473140ab5df2d26a36b6e9ec1f37442d698f71bb
blambin <blambinneng@gmail.ocm>
2015-10-13 17:07:16 +0800

    roengz ban..

--- Room/Form1.cs
+++ Room/Form1.cs
@@ -12,31 +12,70 @@ namespace Room
 {
     public partial class Form1 : Form
     {
+        Location currentLocation;
+        Room diningRoom;
+        RoomWithDoor kitchen;
+        RoomWithDoor livingRoom;
+
+        Outside garden;
+        OutsideWithDoor frontYard;
+        OutsideWithDoor backYard;
+
+
         public Form1()
         {
             InitializeComponent();
+            CreateObjects();
+            MoveTOANewLocation(livingRoom);
         }
 
         
 
         public void CreateObjects()
         {
-            
+            livingRoom = new RoomWithDoor("Living Room", "古董毛毯", "铜把手的橡树门");
+            diningRoom = new Room("Dining Room","水晶吊灯");
+            kitchen = new RoomWithDoor("Kitchen", "不锈钢家具","纱门");
+
+            frontYard = new OutsideWithDoor("Front Yard", true, "铜把手的橡树门");
+            backYard = new OutsideWithDoor("Back Yard", false, "纱门");
+            garden = new Outside("Garden", false);
+
+            diningRoom.Exits = new Location[] { livingRoom, kitchen };
+            livingRoom.Exits = new Location[] { diningRoom };
+            kitchen.Exits = new Location[] { diningRoom };
+
+            frontYard.Exits = new Location[] { backYard, garden };
+            backYard.Exits = new Location[] { frontYard, garden };
+            garden.Exits = new Location[] { backYard, frontYard };
+
+            livingRoom.DoorLocation = frontYard;
+            frontYard.DoorLocation = livingRoom;
+
+            kitchen.DoorLocation = backYard;
+            backYard.DoorLocation = kitchen;
+
+
         }
 
-        private void MoveTOANewLocation(Location location)
+        private void MoveTOANewLocation(Location newLocation)
         {
-            Location currentLocation;
-            currentLocation = location;
+            
+            currentLocation = newLocation;
             comboBoxExits.Items.Clear();
-            comboBoxExits.Items.Add(currentLocation.Exits);
+
+            for (int i = 0; i < currentLocation.Exits.Length; i++)
+            {
+                comboBoxExits.Items.Add(currentLocation.Exits[i].Name);
+            }
             comboBoxExits.SelectedIndex = 0;
 
             textBox1.Text = currentLocation.Description;
 
             if (currentLocation is IHasExteriorDoor)
                 buttonGoThoughTheDoor.Visible = true;
-            buttonGoThoughTheDoor.Visible = false;
+            else
+                buttonGoThoughTheDoor.Visible = false;
 
 
 
@@ -44,12 +83,13 @@ namespace Room
 
         private void buttonGoHere_Click(object sender, EventArgs e)
         {
-            MoveTOANewLocation(comboBoxExits.SelectedIndex);
+            MoveTOANewLocation(currentLocation.Exits[comboBoxExits.SelectedIndex]);
         }
 
         private void buttonGoThoughTheDoor_Click(object sender, EventArgs e)
         {
-
+            IHasExteriorDoor hasDoor = currentLocation as IHasExteriorDoor;
+            MoveTOANewLocation(hasDoor.DoorLocation);
         }
     }
 }
